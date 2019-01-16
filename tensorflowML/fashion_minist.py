570490daeec469b67fb8264fddf2de1bf0f632c4
daring-board <mahotox101500@gmail.com>
2019-01-16 23:19:03 +0900

    2019/01/16

---
+++ tensorflowML/fashion_minist.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = train_images / 255
+test_images = test_images / 255
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(
+    optimizer=tf.train.AdamOptimizer(),
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print(test_loss)
+print(test_acc)
